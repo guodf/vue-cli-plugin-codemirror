96db2589fc179592a9684ec1c5d56791e9b86450
guodf <guo_net_win@live.com>
2018-08-13 23:18:04 +0800

    init

---
+++ generator/index.js
@@ -0,0 +1,8 @@
+module.exports = (api, opts, rootOpts) => {
+
+  api.extendPackage({
+    dependencies: {
+      vue-codemirror: "^4.0.0"
+    }
+  })
+}
---
+++ generator/templates/default/src/plugins/codemirror.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+import VueCodeMirror from 'vuecodemirror';
+
+Vue.use(VueCodeMirror,{});
